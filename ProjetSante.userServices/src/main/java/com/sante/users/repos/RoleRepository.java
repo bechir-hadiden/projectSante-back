625f247f2cc62423518aaf0c13075b379b842b68
bechir <bechirhadiden8@gmail.com>
2024-11-20 13:33:48 +0100

    firstCommit

---
+++ ProjetSante.userServices/src/main/java/com/sante/users/repos/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.sante.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sante.users.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Role findByRole(String role);
+	
+}
